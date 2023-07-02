4bb45886eeeb4d58d25f7dee2485dc5f9a7399b9
ivantselikov@yandex.ru <ivantselikov@yandex.ru>
2023-07-02 23:45:42 +0300

    Добавлен main-файл для запуска на Replit

---
+++ replit.py
@@ -0,0 +1,63 @@
+# файл предназначен для запуска бота на сервере replit
+
+from project_controller import Project
+import config
+from flask import Flask
+from app_logging import get_logger
+from threading import Thread
+import traceback
+
+
+logger = get_logger('main')
+
+logger.info('main.py запущен.')
+
+try:
+    # запуск сервера Flask
+    app = Flask('')
+
+    is_first_request = True
+
+    @app.route('/')
+    def home():
+        try:
+            logger.info('Запрос к серверу.')
+            if project and project.is_alive():
+                if is_first_request:
+                    response = 'Первый успешный запрос к серверу.'
+                    is_first_request = False
+                else:
+                    response = 'Сервер запущен, бот работает.'
+                logger.info(response)
+                return response
+            else:
+                response = 'Бот не отвечает.'
+                logger.warning(response)
+                return response, 500
+        except:
+            response = 'Ошибка сервера.'
+            logger.error(response)
+            logger.error(traceback.format_exc())
+            return response, 500
+
+
+    def run_flask():
+        try:
+            app.run(host='0.0.0.0', port=80)
+        except:
+            logger.error(traceback.format_exc())
+        finally:
+            logger.warning('Сервер Flask остановлен.')
+
+    t = Thread(target=run_flask)
+    t.start()
+
+
+    # запуск бота
+    project = Project(config.DEFAULT_BOT_PATH)
+    project.run(recompile=True, new_console=False)
+
+    logger.warning('Бот остановлен.')
+except:
+    logger.error('Ошибка в main.py')
+    logger.error(traceback.format_exc())
